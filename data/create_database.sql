4a82740c88d7871afef7fe61d46368745a870531
Ana Barba <anabarba111@gmail.com>
2026-04-23 15:08:35 +0200

    ustvarjanje tabel

--- data/create_database.sql
+++ data/create_database.sql
@@ -1,1 +1,40 @@
 -- to je datoteka za ustvarjanje baze podatkov in tabel
+
+CREATE TABLE IF NOT EXISTS oseba
+( 
+    id INTEGER PRIMARY KEY,
+    ime TEXT NOT NULL, 
+    priimek TEXT NOT NULL,
+    elektronski_naslov TEXT NOT NULL UNIQUE,
+    uporabnisko_ime TEXT NOT NULL UNIQUE,
+    geslo TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sladica
+(
+    id INTEGER PRIMARY KEY,
+    ime TEXT NOT NULL,
+    cas_priprave INTEGER NOT NULL,
+    tezavnost () NOT NULL, -- text ali integer?
+    postopek TEXT NOT NULL,
+    kratek_opis TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS kategorija
+(
+    id INTEGER PRIMARY KEY,
+    ime TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS sestavina
+(
+    id INTEGER PRIMARY KEY,
+    ime TEXT NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS pripomocki
+(
+    id INTEGER PRIMARY KEY,
+    ime TEXT NOT NULL
+)
