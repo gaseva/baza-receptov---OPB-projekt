5aa1e3a0b90ef5e6821618cb1af6b08bc884c84d
Ana Barba <anabarba111@gmail.com>
2026-05-04 17:13:42 +0200

    pravice za uporabnike in soclana

---
+++ data/permisions.sql
@@ -0,0 +1,33 @@
+-- dovolimo povezavo in uporabo scheme public javnosti
+GRANT CONNECT ON DATABASE sem2026_anabar TO javnost;
+GRANT USAGE ON SCHEMA public TO javnost;
+
+-- dovolimo vse nekemu konkretnemu uporabniku soclan (WITH GRANT option, dovoli uporabniku dovoljevati pravice)
+GRANT ALL ON DATABASE sem2026_anabar TO evagas WITH GRANT OPTION;
+GRANT ALL ON SCHEMA public TO evagas WITH GRANT OPTION;
+
+-- po ustvarjanju tabel
+GRANT ALL ON ALL TABLES IN SCHEMA public TO evagas WITH GRANT OPTION;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO evagas WITH GRANT OPTION;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO javnost;
+
+-- dodatne pravice za uporabo aplikacije
+GRANT INSERT ON tabela TO javnost;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO javnost;
+
+-- privzete pravice za OBJEKTE, ki jih boš ustvaril v prihodnje
+
+-- nove tabele/pogledi v shemi public
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO evagas WITH GRANT OPTION;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT ON TABLES TO javnost;
+
+-- nove sekvence v shemi public
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON SEQUENCES TO evagas WITH GRANT OPTION;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE ON SEQUENCES TO javnost;
