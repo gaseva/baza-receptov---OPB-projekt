cedbab6cbccacd5ce83b34bc12f873dc8def5750
Eva <gevelin55@gmail.com>
2026-08-25 09:54:27 +0200

    popravek

--- services/sladice_service.py
+++ services/sladice_service.py
@@ -2,9 +2,9 @@ from data.repository import Repository as repo
 
 
 class SladiceService:
-
     def __init__(self):
         self.repository = repo()
 
     def dobi_vse_sladice(self):
         return self.repository.dobi_vse_sladice()
+    
